3b371b8dc1651dab227af9df68ba664b6405669c
dhivya-udhay <68370469+dhivya-udhay@users.noreply.github.com>
2023-10-02 13:42:41 +0530

    Delete EcomDemo directory

--- EcomDemo/src/main/java/com/iamneo/ecom/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iamneo.ecom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.iamneo.ecom.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
--- EcomDemo/target/classes/import.sql
+++
@@ -1,2 +0,0 @@
-insert into products.product
-values(1, 'grocery', 'grocery', 'dal', 234, '1');
